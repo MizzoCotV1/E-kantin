fde4e1e51169c1a9b0667bd5dd8c420ee505d92e
RachmatDjiwandanu <danzyyox0@gmail.com>
2023-09-20 23:21:08 +0700

    awal

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js">
+</head>
+<body>
+<!-- <nav class="navbar navbar-expand-lg bg-body-success"> -->
+<nav class="navbar navbar-expand-lg bg-secondary" data-bs-theme="dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-3">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <ul class = "navbar-nav ms-auto">
+      <li><a class="btn btn-success text-color-dark mx-2" href="#" role="button">Sign Up</a></li>
+      <li><a class="btn btn-success text-color-dark" href="#" role="button">Sign In</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
